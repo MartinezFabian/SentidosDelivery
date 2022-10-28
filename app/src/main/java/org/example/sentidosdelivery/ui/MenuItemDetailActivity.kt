ae615f0553a9db7806bee4e8447559bad1bb2274
MartinezFabian <FabianMartinezDev@gmail.com>
2022-10-28 16:48:38 -0300

    Se agrega pantalla de detalles pedido

---
+++ app/src/main/java/org/example/sentidosdelivery/ui/MenuItemDetailActivity.kt
@@ -0,0 +1,11 @@
+package org.example.sentidosdelivery.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuItemDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_item_detail)
+    }
+}
