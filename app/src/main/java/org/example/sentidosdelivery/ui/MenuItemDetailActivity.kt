6b2a8f416a329a936c3dc24fcc25ccb80ef261ef
MartinezFabian <FabianMartinezDev@gmail.com>
2022-10-31 16:16:42 -0300

    Se agrega funcionalidad de seleccionar cantidad del pedido

--- app/src/main/java/org/example/sentidosdelivery/ui/MenuItemDetailActivity.kt
+++ app/src/main/java/org/example/sentidosdelivery/ui/MenuItemDetailActivity.kt
@@ -1,13 +1,17 @@
 package org.example.sentidosdelivery.ui
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import org.example.sentidosdelivery.R
 import org.example.sentidosdelivery.model.ItemMenu
 import org.example.sentidosdelivery.model.Usuario
 
 class MenuItemDetailActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu_item_detail)
@@ -22,5 +26,30 @@ class MenuItemDetailActivity : AppCompatActivity() {
         tvPrecioMenu.setText("$" + menu!!.precio.toString())
         tvDescripcionMenu.setText(menu!!.ingredientes)
 
+        val btnMas = findViewById<ImageView>(R.id.btnMas)
+        val btnMenos = findViewById<ImageView>(R.id.btnMenos)
+        val tvCantidad = findViewById<TextView>(R.id.tvCantidadDetailActivity)
+
+        var cantidadPedido = 1
+
+        tvCantidad.text = cantidadPedido.toString()
+
+        btnMas.setOnClickListener(View.OnClickListener{
+
+            if(cantidadPedido < 10)
+            {
+                cantidadPedido++
+                tvCantidad.text = cantidadPedido.toString()
+            }
+        })
+
+        btnMenos.setOnClickListener(View.OnClickListener {
+
+            if(cantidadPedido > 1)
+            {
+                cantidadPedido--
+                tvCantidad.text = cantidadPedido.toString()
+            }
+        })
     }
 }
