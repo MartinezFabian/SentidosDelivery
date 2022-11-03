d27781d06121b367911af85dd9f76b300a3a67e3
MartinezFabian <FabianMartinezDev@gmail.com>
2022-11-03 18:39:01 -0300

    Se agrega funcionalidad de calcular el precio del delivery

--- app/src/main/java/org/example/sentidosdelivery/ui/Carrito.kt
+++ app/src/main/java/org/example/sentidosdelivery/ui/Carrito.kt
@@ -1,30 +1,37 @@
 package org.example.sentidosdelivery.ui
 
-import android.annotation.SuppressLint
-import android.content.ClipData.Item
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.google.firebase.database.DataSnapshot
-import com.google.firebase.database.DatabaseError
-import com.google.firebase.database.FirebaseDatabase
-import com.google.firebase.database.ValueEventListener
 import org.example.sentidosdelivery.R
 import org.example.sentidosdelivery.adapter.CarritoAdapter
 import org.example.sentidosdelivery.model.ItemMenu
 
 public var lista_carrito: ArrayList<ItemMenu> = arrayListOf<ItemMenu>()
 
-class Carrito : Fragment() {
+class Carrito : Fragment(), RadioGroup.OnCheckedChangeListener {
 
     private lateinit var carritoRecyclerView: RecyclerView
     private val carritoAdapter: CarritoAdapter = CarritoAdapter()
     private val menuItemDetailActivity:MenuItemDetailActivity = MenuItemDetailActivity()
+
+    var tvPrecioTotal: TextView? = null
+    var precioTotal: Int = 0
+
+    var radioGroup: RadioGroup? = null
+    var rbEnvioAdomicilio: RadioButton? = null
+    var rbRetirarEnRestaurante: RadioButton? = null
+    var tvPrecioDelivery: TextView? = null
+    var precioDelivery = 0.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -50,17 +57,43 @@ class Carrito : Fragment() {
         carritoAdapter.notifyDataSetChanged()
 
         //Calcular Precio Total
-        val tvPrecioTotal: TextView = rootView.findViewById(R.id.tvPrecioTotal)
-        var precioTotal: Int = 0
+        tvPrecioTotal = rootView.findViewById(R.id.tvPrecioTotal)
+        precioTotal = 0
 
         for (item in lista_carrito)
         {
             precioTotal += item.precio
         }
 
-        tvPrecioTotal.setText("$"+precioTotal.toString())
+        if(lista_carrito.isNotEmpty())
+        {
+            tvPrecioTotal!!.setText("$"+precioTotal.toString())
+        }
+
+        //Calcular Precio delivery
+        tvPrecioDelivery = rootView.findViewById(R.id.tvPrecioDelivery)
+        rbRetirarEnRestaurante= rootView.findViewById(R.id.rbRetirarEnRestaurante)
+        rbEnvioAdomicilio = rootView.findViewById(R.id.rbEnvioAdomicilio)
+        radioGroup = rootView.findViewById(R.id.radio)
+
+        radioGroup?.setOnCheckedChangeListener(this)
+
 
         return rootView
+    }
+
+    override fun onCheckedChanged(group: RadioGroup?, checkedId: Int) {
+        when(checkedId){
+            rbRetirarEnRestaurante?.id -> {
+                tvPrecioDelivery!!.setText("$0.0")
+            }
+
+            rbEnvioAdomicilio?.id -> {
+                precioDelivery = precioTotal * 0.01
+
+                tvPrecioDelivery!!.setText("$ %.2f".format(precioDelivery))
+            }
+        }
     }
 
 }
